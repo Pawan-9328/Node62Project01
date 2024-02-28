2e99eed4827defe04d19dcc77c9ec870af459871
Pawan Gola <pawangola908@gmail.com>
2024-02-28 18:52:51 +0530

    patch complete

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 //...Rest Api First Project - 01...
 const express = require("express");
 const users = require('./MOCK_DATA.json')
+
 const app = express();
 const PORT = 8000;
 
 //..Routes
-app.get('/api/users', (req, res) =>{
-    return res.json(users);
+app.get('/users', (req, res) => {
+   const html = `
+    <ul>
+    ${users.map(user => `<li> ${user.first_name}  </li>`)}
+   `;
+   res.send(html);
 });
 
-app.get('/users', (req, res) =>{
-   
+//...REST API...
+app.get('/api/users', (req, res) => {
+   return res.json(users);
 });
 
-app.listen(PORT, () =>console.log(`Server Started at Port ${PORT}`)
+app.get("/api/users/:id", (req, res) => {
+   const id = Number(req.params.id);
+   const user = users.find((user) => user.id === id);
+   return res.json(user);
+});
+
+app.post('/api/users', (req, res) =>{
+    //..TODO: Create new User 
+    return res.json({status: "pending"});
+});
+
+app.patch('/api/users', (req, res) =>{
+   //..TODO: CEdit the user with id 
+   return res.json({status: "pending"});
+});
+
+app.listen(PORT, () => console.log(`Server Started at Port ${PORT}`)
 );
