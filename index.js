0b992cb9f045b55e079281415b116790d81b4919
Pawan Gola <pawangola908@gmail.com>
2024-02-29 12:00:47 +0530

    now complete first restapi project

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //...Rest Api First Project - 01...
 const express = require("express");
+const fs = require('fs');
 const users = require('./MOCK_DATA.json')
 
 const app = express();
 const PORT = 8000;
+
+//...Middleware - plugin....
+app.use(express.urlencoded({ extended: false }));
+
 
 //..Routes
 app.get('/users', (req, res) => {
@@ -19,30 +24,39 @@ app.get('/api/users', (req, res) => {
    return res.json(users);
 });
 
-//..merge id [ create only one id.....]
+//..merge id [ create only one id]..
 //called grouping
-app.route('/api/users/:id').get((req, res) =>{{
-   const id = Number(req.params.id);
-   const user = users.find((user) => user.id === id);
-   return res.json(user);
-}})
-.put((req, res) => {
-   //Edit user with id 
-  return res.json({status: 'Pending'});
+
+app.route('/api/users/:id').get((req, res) => {
+   {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+      return res.json(user);
+   }
 })
 
-.delete((req, res) => {
-  return res.json({status: 'Delete'});
+   .put((req, res) => {
+      //Edit user with id 
+      return res.json({ status: 'Pending' });
+   })
 
-})
+   .delete((req, res) => {
+      return res.json({ status: 'Delete' });
+
+   })
 
 // app.get("/api/users/:id", (req, res) => {
-   
+
 // });
 
-app.post('/api/users/:id', (req, res) =>{
-    //..TODO: Create new User 
-    return res.json({status: "pending"});
+app.post('/api/users/:id', (req, res) => {
+   const body = req.body;
+   users.push({...body, id: users.length + 1});
+   fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+      return res.json({ status: "success", id: users.length + 1 });
+   })
+
+
 })
 
 
